62b2cd48c8c6f28cb3feaf8f9d07d3504a90e66e
Joshua Lum <joshuanl@usc.edu>
2015-02-07 20:33:07 -0800

    bfs works

--- joshuanl_CSCI201_Assignment02/src/assignment02/Battleship.java
+++ joshuanl_CSCI201_Assignment02/src/assignment02/Battleship.java
@@ -30,6 +30,8 @@ import javax.swing.JTextField;
 		private JLabel scoreCountLabel;
 		private int scoreCount;
 		protected static int board[][][];
+		protected static boolean visited[][];
+		protected static Player playerList[];
 		
 		
 		public Battleship(){
@@ -102,15 +104,15 @@ import javax.swing.JTextField;
 			add(flowPanel, BorderLayout.SOUTH);
 //=========================================CREATING HIGHSCORES PANEL			
 			JLabel tempLabel;
-			JPanel highscoreGirdPanel = new JPanel();
-			highscoreGirdPanel.setLayout(new GridLayout(11,1));
+			JPanel highscoreBoxPanel = new JPanel();
+			highscoreBoxPanel.setLayout(new BoxLayout(highscoreBoxPanel, BoxLayout.Y_AXIS));
 			tempLabel = new JLabel("Highscores                    ");
-			highscoreGirdPanel.add(tempLabel);
+			highscoreBoxPanel.add(tempLabel);
 			for(int i=1; i<=10; i++){
 				highscores[(i-1)] = new JLabel(i+".     ");
-				highscoreGirdPanel.add(highscores[(i-1)]);
+				highscoreBoxPanel.add(highscores[(i-1)]);
 			}
-			add(highscoreGirdPanel, BorderLayout.EAST);
+			add(highscoreBoxPanel, BorderLayout.EAST);
 			
 			
 //===================================END OF CONSTRUCTOR			
@@ -154,16 +156,22 @@ import javax.swing.JTextField;
 			Battleship BS = new Battleship();
 			//BS.setVisible(true);
 			boolean accept_input = false;
+			visited  = new boolean[10][10];
 			String filename, input;
-			String highscoreNames[] = new String[10];
 			Scanner keyboard = new Scanner(System.in);
 			FileReader fr;
 			BufferedReader br = null;
-			board = new int[11][11][1]; 
+			board = new int[10][10][1]; 
+			playerList = new Player[11];
 			int ship2Count = 2, ship3Count = 1, ship4Count = 1, ship5Count = 1;
 			boolean validBoard = false;
 			JLabel tempLabel;
-			
+	//============================ INIT arrays		
+			Player temp_player;
+			for(int i=0; i<10; i++){
+				temp_player = new Player();
+				playerList[i] = temp_player;
+			}
 			
 	//=============================READING FILENAME
 			while(!accept_input){
@@ -200,13 +208,43 @@ import javax.swing.JTextField;
 								if((temp_int-1) == i){
 									if(st.hasMoreTokens()){
 										temp_str = st.nextToken();
-										highscoreNames[i] = temp_str;
-										highscores[i].setText((i+1)+".     "+temp_str);
-										
+										temp_player = new Player(temp_str);
+										System.out.println("=============grabbed player name: "+temp_player.getName());
+										//highscores[i].setText((i+1)+".     "+temp_str);
+										if(st.hasMoreTokens()){
+											temp_str = st.nextToken();
+											if(temp_str.charAt(0) == '-'){
+												if (st.hasMoreTokens()) {
+													temp_str = st.nextToken();
+													if(isInteger(temp_str)){
+														temp_player.setScore(Integer.parseInt(temp_str));
+														System.out.println("===================players score: "+temp_player.getScore());
+														playerList[i] = temp_player;
+														highscores[i].setText((i+1)+".     "+playerList[i].getName() + " - "+playerList[i].getScore());
+													}
+													else{
+														System.out.println("Invalid input in file, found token but not integer");
+														System.exit(0);
+													}
+												}//end of if has token for score
+												else{
+													System.out.println("Invalid input in file, found - but no token");
+													System.exit(0);
+												}
+											}//end of equals "-"
+											else{
+												System.out.println("Invalid input in file, found token but not -");
+												System.exit(0);
+											}
+										}//end of if theres token "-"
+										else{
+											System.out.println("Invalid input in file, didnt find -");
+											System.exit(0);
+
+										}
 									}//end of if theres a name
 									else{
-										highscoreNames[i] = "";
-										highscores[i].setText((i+1)+".     "+highscoreNames[i]);
+										highscores[i].setText((i+1)+".     "+playerList[i].getName());
 									}//end of else theres no name
 								}//end of if correct integer
 								else{
@@ -280,110 +318,170 @@ import javax.swing.JTextField;
 				System.out.println();
 			}//end of outer for
 //====================================CHECKING FOR VALID BOARD			
-			boolean visited[][] = new boolean[10][10];
-			switch(BFS(board, visited, 0, 0, 0, 0)){
-				case -1:
-					break;
-				case 2:
-					ship2Count--;
-					break;
-				case 3:
-					ship3Count--;
-					break;
-				case 4:
-					ship4Count--;
-					break;
-				case 5:
-					ship5Count--;
-					break;
-			}//end of checking ships
-			if(ship2Count == 0 && ship3Count == 0 && ship4Count == 0 && ship5Count == 0){
-				validBoard = true;
-				System.out.println("Valid board");
-			}//end of check
-			else{
-				System.out.println("not valid board");
-			}
+			//boolean visited[][] = new boolean[10][10];
+//			switch(BFS(board, visited, 0, 0, 0, 0)){
+//				case -1:
+//					break;
+//				case 2:
+//					ship2Count--;
+//					break;
+//				case 3:
+//					ship3Count--;
+//					break;
+//				case 4:
+//					ship4Count--;
+//					break;
+//				case 5:
+//					ship5Count--;
+//					break;
+//			}//end of checking ships
+//			if(ship2Count == 0 && ship3Count == 0 && ship4Count == 0 && ship5Count == 0){
+//				validBoard = true;
+//				System.out.println("Valid board");
+//			}//end of check
+//			else{
+//				System.out.println("not valid board");
+//			}
 //==============================================END OF BOARD CHECK	
-			
-			
+			for(int i=0; i < 10; i++){
+				for(int j=0; j < 10; j++){
+					if(board[i][j][0] != 0 && !visited[i][j]){
+						//System.out.println("ran into ship: "+board[i][j][0]);
+						visited[i][j] = true;
+						int result = BFS(board, i, j, board[i][j][0], 1);
+						System.out.println("result: "+result);
+						switch(result){
+							case 2:
+								ship2Count--;
+							break;
+							case 3:
+								ship3Count--;
+							break;
+							case 4:
+								ship4Count--;
+							break;
+							case 5:
+								ship5Count--;
+							break;
+							default:
+								System.out.println("default");
+							break;	
+						}//end of switch	
+					}//end of else not water
+				}//end of inner for
+			}//end of outer for
+			if(ship2Count == 0 && ship3Count == 0 && ship4Count == 0 && ship5Count == 0){
+				System.out.println("valid board");
+			}
+			else{
+				System.out.println("no valid board");
+				System.out.println("ship2: " + ship2Count);
+				System.out.println("ship3: " + ship3Count);
+				System.out.println("ship4: " + ship4Count);
+				System.out.println("ship5: " + ship5Count);
+			}
 			BS.setVisible(true);
 		}//end of main
 //===============================================METHODS		
-//===============================================IS INTEGER		
-		
-//===================================================BFS
-		public static int BFS(int board[][][], boolean visited[][], int i, int j, int ship, int length){
-			
-			if(i > 9 || j > 9){
-				return -1;
-			}//end of if out of bounds
-			if(visited[i][j]){
-				//System.out.println("["+i+"]["+j+"]");
-				return -1;
-			}//end of if not visited
-			else{	
-				visited[i][j] = true;
+		public static int BFS(int board[][][], int i, int j, int ship, int length){
+			System.out.println("ship: " + ship);
+			System.out.println("length: " + length);
+			if(ship == length){
+				System.out.println("=============found valid ship: "+ship);
+				return ship;
 			}
-			
-			switch(board[i][j][0]){
-				case 0:
-					System.out.println("found water");
-					System.out.println("["+i+"]["+j+"]");
-					if((j+1) <= 9){
-						return BFS(board, visited, i, j+1, 0, 0);
-					}
-					else{
-						j = 0;
-						i++;
-						return BFS(board, visited, i, j, 0, 0);
-					}
-				case 2:
-					length++;
-					System.out.println("Ship2, Length: "+length);
-					System.out.println("["+i+"]["+j+"]");
-					BFS(board, visited, i+1, j, 2, length);
-					BFS(board, visited, i, j+1, 2, length);
-					if(ship == 2 && length == 2){
-						System.out.println("=======Found Ship 2");
-						return 2;
-					}
-					break;
-				case 3:	
-					length++;
-					System.out.println("Ship3, Length: "+length);
-					System.out.println("["+i+"]["+j+"]");
-					BFS(board, visited, i+1, j, 3, length);
-					BFS(board, visited, i, j+1, 3, length);
-					if(ship == 3 && length == 3){
-						System.out.println("=======Found Ship 3");
-						return 3;
-					}	
-					break;
-				case 4:
-					length++;
-					System.out.println("Ship4, Length: "+length);
-					System.out.println("["+i+"]["+j+"]");
-					BFS(board, visited, i+1, j, 4, length);
-					BFS(board, visited, i, j+1, 4, length);
-					if(ship == 4 && length == 4){
-						System.out.println("======Found Ship 4");
-						return 4;
-					}	
-					break;
-				case 5:
-					length++;
-					System.out.println("Ship5, Length: "+length);
-					System.out.println("["+i+"]["+j+"]");
-					BFS(board, visited, i+1, j, 5, length);
-					BFS(board, visited, i, j+1, 5, length);
-					if(ship == 5 && length == 5){
-						System.out.println("=======Found Ship 5");
-						return 5;
-					}	
-					break;
-			}//end of switch		
-			
-			return -1;
-		}//end of BFS
+			if((j+1) < 10){
+				System.out.println("in j bounds");
+				if(board[i][j+1][0] == ship && !visited[i][j+1]){
+					System.out.println("found ship to right");
+					visited[i][j+1] = true;
+					return BFS(board, i, (j+1), ship, ++length);
+					//return -1;
+				}
+			}//end of if in bounds to right	
+			if((i+1) < 10){
+				System.out.println("in i bounds");
+				if(board[i+1][j][0] == ship && !visited[i+1][j]){
+					System.out.println("found ship to down");
+					visited[i+1][j] = true;
+					return BFS(board, (i+1), j, ship, ++length);
+					//return -1;
+				}
+			}//end of if in bounds to down
+			return 0;
+		}
+//===================================================BFS
+//		public static int BFS(int board[][][], boolean visited[][], int i, int j, int ship, int length){
+//			
+//			if(i > 9 || j > 9){
+//				return -1;
+//			}//end of if out of bounds
+//			if(visited[i][j]){
+//				//System.out.println("["+i+"]["+j+"]");
+//				return -1;
+//			}//end of if not visited
+//			else{	
+//				visited[i][j] = true;
+//			}
+//			
+//			switch(board[i][j][0]){
+//				case 0:
+//					System.out.println("found water");
+//					System.out.println("["+i+"]["+j+"]");
+//					if((j+1) <= 9){
+//						return BFS(board, visited, i, j+1, 0, 0);
+//					}
+//					else{
+//						j = 0;
+//						i++;
+//						return BFS(board, visited, i, j, 0, 0);
+//					}
+//				case 2:
+//					length++;
+//					System.out.println("Ship2, Length: "+length);
+//					System.out.println("["+i+"]["+j+"]");
+//					BFS(board, visited, i+1, j, 2, length);
+//					BFS(board, visited, i, j+1, 2, length);
+//					if(ship == 2 && length == 2){
+//						System.out.println("=======Found Ship 2");
+//						return 2;
+//					}
+//					break;
+//				case 3:	
+//					length++;
+//					System.out.println("Ship3, Length: "+length);
+//					System.out.println("["+i+"]["+j+"]");
+//					BFS(board, visited, i+1, j, 3, length);
+//					BFS(board, visited, i, j+1, 3, length);
+//					if(ship == 3 && length == 3){
+//						System.out.println("=======Found Ship 3");
+//						return 3;
+//					}	
+//					break;
+//				case 4:
+//					length++;
+//					System.out.println("Ship4, Length: "+length);
+//					System.out.println("["+i+"]["+j+"]");
+//					BFS(board, visited, i+1, j, 4, length);
+//					BFS(board, visited, i, j+1, 4, length);
+//					if(ship == 4 && length == 4){
+//						System.out.println("======Found Ship 4");
+//						return 4;
+//					}	
+//					break;
+//				case 5:
+//					length++;
+//					System.out.println("Ship5, Length: "+length);
+//					System.out.println("["+i+"]["+j+"]");
+//					BFS(board, visited, i+1, j, 5, length);
+//					BFS(board, visited, i, j+1, 5, length);
+//					if(ship == 5 && length == 5){
+//						System.out.println("=======Found Ship 5");
+//						return 5;
+//					}	
+//					break;
+//			}//end of switch		
+//			
+//			return -1;
+//		}//end of BFS
 }//end of class
---
+++ joshuanl_CSCI201_Assignment02/src/assignment02/Player.java
@@ -0,0 +1,28 @@
+package assignment02;
+
+public class Player {
+	private String player_name;
+	private int score;
+	
+	public Player(String player_name){
+		this.player_name = player_name;
+	}//end of constructor
+	
+	public Player(){
+		player_name = "";
+		score = -1;
+	}//end of default constructor
+	
+	public String getName(){
+		return player_name;
+	}//end of getting the name of the player
+	
+	public int getScore(){
+		return score;
+	}//end of getting score method
+	
+	public void setScore(int score){
+		this.score = score;
+	}//end of setting score method
+	
+}//end of class
